40d90a0e27b6d3aa53332e3af87f8182086dd93f
HP <HP@DESKTOP-2SKVUE2>
2023-08-26 22:29:04 +0530

    new

--- src/main/java/genericLab/Screenshot.java
+++ src/main/java/genericLab/Screenshot.java
@@ -1,6 +1,6 @@
 package genericLab;
 
-import java.io.File;
+import java.io.File; 
 import java.io.IOException;
 import java.util.Date;
 
@@ -8,16 +8,18 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
 
 public class Screenshot {
 public void getPhoto(WebDriver driver,String name) throws IOException {
 	
 	Date d=new Date();
-	String currentdate= d.toString().replace(";", "-");
+	String currentdate= d.toString().replace(":", "_");
 	TakesScreenshot ts=(TakesScreenshot)driver;
 	File src=ts.getScreenshotAs(OutputType.FILE);
-	File dest= new File(AutoConstant.photopath+currentdate+name+".png");
+	File dest= new File(AutoConstant.photopath+name+currentdate+".png");
 	FileUtils.copyFile(src, dest);
+	Reporter.log("<img src='"+dest+"' height='400' width='400' /> ");
 
 }
 }
